c625eba8fe096a4ce62756652dd5e188d7bb30a1
Nguyen Cong Binh <ncbinh01@gmail.com>
2020-06-15 16:26:42 +0700

    [Update] Update constants

--- pages/trades/index.vue
+++ pages/trades/index.vue
@@ -1,13 +1,40 @@
 <template>
-  <div>
-    Trades here
+  <div class="ma-3">
+    <v-data-table
+      :headers="headers"
+      :items="desserts"
+      class="elevation-1"
+    >
+      <template v-slot:item.nO="{ item, index }">
+        <span>{{ index }}</span>
+      </template>
+    </v-data-table>
   </div>
 </template>
 
 <script>
 export default {
-  // middleware: ["isNotAuth"]
-};
+  middleware: ["isNotAuth"],
+  data(){
+    return {
+      headers: [
+          {
+            text: 'STT',
+            sortable: false,
+            value: 'nO',
+          },
+          { text: 'Trade Date', value: 'tradeDate' },
+          { text: 'Order', value: 'order' },
+          { text: 'Market', value: 'market' },
+          { text: 'Price one', value: 'point_one' },
+          { text: 'Stop lost', value: 'sl' },
+          { text: 'Take profit(quick)', value: 'tp_short' },
+          { text: 'Take profit(low)', value: 'tp_long' },
+          { text: 'Status', value: 'status' },
+        ],
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped></style>
--- utils/constants.js
+++ utils/constants.js
@@ -1,1 +1,3 @@
 export const LOCALSTORAGE_TOKEN_NAME = 'bn-token'
+
+export const AUTHOR_NAME = 'Binh Nguyen 2020'
